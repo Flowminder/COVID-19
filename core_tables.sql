423e240316e557d7d3cf36c76d05b9adca7265f5
James Harrison <james.harrison@flowminder.org>
2020-03-25 13:04:09 +0000

    Add comment in core_tables.sql

--- core_tables.sql
+++ core_tables.sql
@@ -7,5 +7,5 @@ CREATE TABLE calls(
 
 CREATE TABLE cells(
     cell_id TEXT,
-    region TEXT
+    region TEXT -- We are able to provide assistance with mapping cell tower locations to administrative (or other) regions, if you do not already have this mapping.
 );
