f4a0a2551d3801b1423961cbf2018ec1b54d245d
James Harrison <james.harrison@flowminder.org>
2020-03-26 13:26:28 +0000

    Filter out counts <= 15

--- indicator_5.sql
+++ indicator_5.sql
@@ -1,44 +1,47 @@
 CREATE TABLE regional_pair_connections_per_day AS (
 
-    SELECT date,
-        region1,
-        region2,
-        COUNT(*)
-    FROM (
-
-        SELECT t1.date AS date,
-            t1.msisdn AS msisdn,
-            t1.region AS region1,
-            t2.region AS region2
+    SELECT * FROM (
+        SELECT date,
+            region1,
+            region2,
+            COUNT(*) AS count
         FROM (
-            SELECT calls.msisdn,
-                cells.region
-            FROM calls
-            INNER JOIN cells
-                ON calls.location_id = cells.cell_id
-    	      WHERE calls.date >= '2020-03-01'
-    	          AND calls.date <= CURRENT_DATE
-            GROUP BY 1
-            ) t1
 
-            FULL OUTER JOIN
+            SELECT t1.date AS date,
+                t1.msisdn AS msisdn,
+                t1.region AS region1,
+                t2.region AS region2
+            FROM (
+                SELECT calls.msisdn,
+                    cells.region
+                FROM calls
+                INNER JOIN cells
+                    ON calls.location_id = cells.cell_id
+                WHERE calls.date >= '2020-03-01'
+                    AND calls.date <= CURRENT_DATE
+                GROUP BY 1
+                ) t1
 
-            (
-            SELECT calls.msisdn,
-    	          cells.region
-            FROM calls
-            INNER JOIN cells
-                ON calls.location_id = cells.cell_id
-    	      WHERE calls.date >= '2020-03-01'
-    	          AND calls.date <= CURRENT_DATE
-            GROUP BY 1
-            ) t2
+                FULL OUTER JOIN
 
-            ON t1.msisdn = t2.msisdn
-    	      AND t1.date = t2.date
-        WHERE t1.region < t2.region
+                (
+                SELECT calls.msisdn,
+                    cells.region
+                FROM calls
+                INNER JOIN cells
+                    ON calls.location_id = cells.cell_id
+                WHERE calls.date >= '2020-03-01'
+                    AND calls.date <= CURRENT_DATE
+                GROUP BY 1
+                ) t2
 
-    )
-    GROUP BY 1, 2, 3
+                ON t1.msisdn = t2.msisdn
+                AND t1.date = t2.date
+            WHERE t1.region < t2.region
+
+        )
+        GROUP BY 1, 2, 3
+    ) AS grouped
+    WHERE grouped.count > 15
 
 );
