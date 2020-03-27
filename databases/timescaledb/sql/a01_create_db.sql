6ad5b6a54794e7322b46782691cad22b1bd70d5e
chrisjbrooks <chris.brooks@flowminder.org>
2020-03-27 14:10:07 +0000

    add license headers to timescaledb example files

--- databases/timescaledb/sql/a01_create_db.sql
+++ databases/timescaledb/sql/a01_create_db.sql
@@ -1,4 +1,8 @@
 /*
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 EVENTS ---------------------------------------------------
 This schema collection organizes data provided by operators
 into a predictable format.
--- databases/timescaledb/sql/b01_create_sample_procedure.sql
+++ databases/timescaledb/sql/b01_create_sample_procedure.sql
@@ -1,4 +1,8 @@
 /*
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 ADD_SAMPLE_GENERATOR---------------------------------------
 Create a function to generate synthetic sample CDR data
 @chrisjbrooks
